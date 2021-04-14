1657985b15ca73394efdc3dbdc8b394d1f5436a6
Adam Holecska <39831883+holecska@users.noreply.github.com>
2021-04-14 18:15:43 +0200

    deleted unnecessary lines

--- main.js
+++ main.js
@@ -7,13 +7,3 @@ function closeNav() {
   document.getElementById("menu").style.display = "none";
   document.getElementById("header-big").style.display = "grid";
 }
-
-// function contactDisplay() {
-//   var w = window.innerWidth;
-//   if (w>600){
-//     document.getElementById("contact").style.display = "block";
-//   } else{
-//     document.getElementById("contact").style.display = "none";
-//   }
-// }
-//onresize="contactDisplay()"
